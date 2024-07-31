85fd92ba466cac66016b96a06853276c98d16fa9
fadhilmuhamaadd <121470307+fadhilmuhamaadd@users.noreply.github.com>
2024-07-31 09:46:56 +0700

    update

--- resources/views/analisis_desain/dokumen.blade.php
+++ resources/views/analisis_desain/dokumen.blade.php
@@ -52,92 +52,90 @@
 <body>
 
 @foreach($dataanalisis as $analisis)
-    <div class="header">
-        <table>
-            <tr>
-                <td rowspan="4">
-                    <img src="{{ asset('path_to_logo_image.png') }}" alt="Logo" width="100">
-                </td>
-                <td rowspan="4" class="text-center">
-                    <h2>ANALISIS & DESAIN SISTEM INFORMASI</h2>
-                </td>
-                <td>No. Dokumen</td>
-                <td>FP-DTI03-08</td>
-            </tr>
-            <tr>
-                <td>No. Revisi</td>
-                <td>0</td>
-            </tr>
-            <tr>
-                <td>Tanggal Revisi</td>
-                <td>2024</td>
-            </tr>
-            <tr>
-                <td>Halaman</td>
-                <td>1</td>
-            </tr>
-        </table>
-    </div>
+<div class="header">
+    <table>
+        <tr>
+            <td rowspan="4">
+                <img src="{{ asset('path_to_logo_image.png') }}" alt="Logo" width="100">
+            </td>
+            <td rowspan="4" class="text-center">
+                <h2>ANALISIS & DESAIN SISTEM INFORMASI</h2>
+            </td>
+            <td>No. Dokumen</td>
+            <td>FP-DTI03-08</td>
+        </tr>
+        <tr>
+            <td>No. Revisi</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>Tanggal Revisi</td>
+            <td>2024</td>
+        </tr>
+        <tr>
+            <td>Halaman</td>
+            <td>1</td>
+        </tr>
+    </table>
+</div>
 
-    <div class="bordered">
-        <table class="table-container">
-            <tr>
-                <th>Nomor Proyek</th>
-                <td>xxxxxxxxxxxxx (Nomor Proyek Perlu disepakati)</td>
-            </tr>
-            <tr>
-                <th>Nama Proyek</th>
-                <td>{{ $analisis->nama_proyek }}</td>
-            </tr>
-            <tr>
-                <th>Deskripsi</th>
-                <td>{{ $analisis->deskripsi_proyek }}</td>
-            </tr>
-            <tr>
-                <th>Manajer Proyek</th>
-                <td>{{ $analisis->manajer_proyek }}</td>
-            </tr>
-            <tr>
-                <th>Kebutuhan Fungsional dan Deskripsi</th>
-                <td>{{ $analisis->kebutuhan_fungsi }}</td>
-            </tr>
-            <tr>
-                <th>Arsitektur Sistem Informasi</th>
-                <td>{{ $analisis->gambaran_arsitektur }}</td>
-            </tr>
-            <tr>
-                <th>Desain Detil</th>
-                <td>{{ $analisis->detil_arsitektur }}</td>
-            </tr>
-            <tr>
-                <th>Lampiran</th>
-                <td>{{ $analisis->lampiran_mockup }}</td>
-            </tr>
-        </table>
+<table>
+    <tr>
+        <th>Nomor Proyek</th>
+        <td>xxxxxxxxxxxxx (Nomor Proyek Perlu disepakati)</td>
+    </tr>
+    <tr>
+        <th>Nama Proyek</th>
+        <td>{{ $analisis->nama_proyek }}</td>
+    </tr>
+    <tr>
+        <th>Deskripsi</th>
+        <td>{{ $analisis->deskripsi_proyek }}</td>
+    </tr>
+    <tr>
+        <th>Manajer Proyek</th>
+        <td>{{ $analisis->manajer_proyek }}</td>
+    </tr>
+    <tr>
+        <th>Kebutuhan Fungsional dan Deskripsi</th>
+        <td>{{ $analisis->kebutuhan_fungsi }}</td>
+    </tr>
+    <tr>
+        <th>Arsitektur Sistem Informasi</th>
+        <td>{{ $analisis->gambaran_arsitektur }}</td>
+    </tr>
+    <tr>
+        <th>Desain Detil</th>
+        <td>{{ $analisis->detil_arsitektur }}</td>
+    </tr>
+    <tr>
+        <th>Lampiran</th>
+        <td>{{ $analisis->lampiran_mockup }}</td>
+    </tr>
+</table>
 
-        <table class="table">
-            <tr>
-                <th class="text-center" colspan="2">Disiapkan oleh</th>
-                <th class="text-center" colspan="2">Disetujui oleh</th>
-            </tr>
-            <tr>
-                <td colspan="2" style="height: 100px;"></td>
-                <td colspan="2" style="height: 100px;"></td>
-            </tr>
-            <tr>
-                <td class="text-center" colspan="2">{{ $analisis->nama_pemohon }}<br>{{ $analisis->jabatan_pemohon }}</td>
-                <td class="text-center" colspan="2">{{ $analisis->nama }}<br>{{ $analisis->jabatan }}</td>
-            </tr>
-            <tr>
-                <td class="text-center" colspan="2">Tanggal: {{ \Carbon\Carbon::parse($analisis->tanggal_disiapkan)->format('d-m-Y') }}</td>
-                <td class="text-center" colspan="2">Tanggal: {{ \Carbon\Carbon::parse($analisis->tanggal_disetujui)->format('d-m-Y') }}</td>
-            </tr>
-        </table>
-    </div>
+<table class="table">
+    <tr>
+        <th class="text-center" colspan="2">Disiapkan oleh</th>
+        <th class="text-center" colspan="2">Disetujui oleh</th>
+    </tr>
+    <tr>
+        <td colspan="2" style="height: 100px;"></td>
+        <td colspan="2" style="height: 100px;"></td>
+    </tr>
+    <tr>
+        <td class="text-center" colspan="2">{{ $analisis->nama_pemohon }}<br>{{ $analisis->jabatan_pemohon }}</td>
+        <td class="text-center" colspan="2">{{ $analisis->nama }}<br>{{ $analisis->jabatan }}</td>
+    </tr>
+    <tr>
+        <td class="text-center" colspan="2">Tanggal: {{ \Carbon\Carbon::parse($analisis->tanggal_disiapkan)->format('d-m-Y') }}</td>
+        <td class="text-center" colspan="2">Tanggal: {{ \Carbon\Carbon::parse($analisis->tanggal_disetujui)->format('d-m-Y') }}</td>
+    </tr>
+</table>
 
-    @if (!$loop->last)
-    <div class="page-break"></div>
-    @endif
+@if (!$loop->last)
+<div class="page-break"></div>
+@endif
 @endforeach
 
 </body>
--- resources/views/persetujuan_pengembangan/dokumen.blade.php
+++ resources/views/persetujuan_pengembangan/dokumen.blade.php
@@ -12,38 +12,37 @@
             color: #333;
         }
         .header {
-            text-align: center;
             margin-bottom: 20px;
         }
-        .header h1 {
-            margin: 0;
-        }
-        .header p {
+        .header h2 {
             margin: 5px 0;
         }
-        .table-container {
+        table {
             width: 100%;
             border-collapse: collapse;
             margin-bottom: 20px;
         }
-        .table-container, .table-container th, .table-container td {
+        table, th, td {
             border: 1px solid black;
         }
-        .table-container th, .table-container td {
+        th, td {
             padding: 8px;
             text-align: left;
         }
-        .section-title {
-            font-weight: bold;
-            margin-top: 20px;
+        .no-border {
+            border: none;
         }
-        .bordered {
-            border: 1px solid #333;
-            padding: 10px;
-            margin-bottom: 20px;
+        .text-center {
+            text-align: center;
         }
         .text-right {
             text-align: right;
+        }
+        .text-left {
+            text-align: left;
+        }
+        .bold {
+            font-weight: bold;
         }
         .page-break {
             page-break-after: always;
@@ -51,104 +50,107 @@
     </style>
 </head>
 <body>
-    <div class="header">
-        <h1>Persetujuan Permintaan Pengembangan Sistem Informasi</h1>
-        <p>No. Dokumen: FP-DTI03-0A</p>
-        <p>No. Revisi: 0</p>
-        <p>Tanggal Revisi: 2024</p>
-        <p>Halaman: 1</p>
-    </div>
 
-    @foreach ($datapersetujuan as $persetujuan)
-        <div class="bordered">
-            <table class="table-container">
-                <tr>
-                    <th>Latar Belakang</th>
-                    <td>{{ $persetujuan->latar_belakang }}</td>
-                </tr>
-                <tr>
-                    <th>Tujuan</th>
-                    <td>{{ $persetujuan->tujuan }}</td>
-                </tr>
-                <tr>
-                    <th>Target Implementasi Sistem</th>
-                    <td>{{ $persetujuan->target_implementasi }}</td>
-                </tr>
-                <tr>
-                    <th>Fungsi-fungsi Sistem Informasi</th>
-                    <td>{{ $persetujuan->fungsi_sistem }}</td>
-                </tr>
-                <tr>
-                    <th>Jenis Aplikasi</th>
-                    <td>{{ $persetujuan->jenis_aplikasi }}</td>
-                </tr>
-                <tr>
-                    <th>Pengguna</th>
-                    <td>{{ $persetujuan->pengguna }}</td>
-                </tr>
-                <tr>
-                    <th>Uraian Permintaan Tambahan/Khusus</th>
-                    <td>{{ $persetujuan->uraian_tambahan }}</td>
-                </tr>
-            </table>
+@foreach($datapersetujuan as $persetujuan)
+<div class="header">
+    <table>
+        <tr>
+            <td rowspan="4">
+                <img src="{{ asset('path_to_logo_image.png') }}" alt="Logo" width="100">
+            </td>
+            <td rowspan="4" class="text-center">
+                <h2>Persetujuan Permintaan</h2>
+                <h2>Pengembangan Sistem Informasi</h2>
+            </td>
+            <td>No. Dokumen</td>
+            <td>FP-DTI03-08</td>
+        </tr>
+        <tr>
+            <td>No. Revisi</td>
+            <td>0</td>
+        </tr>
+        <tr>
+            <td>Tanggal Revisi</td>
+            <td>2024</td>
+        </tr>
+        <tr>
+            <td>Halaman</td>
+            <td>1</td>
+        </tr>
+    </table>
+</div>
 
-            <div class="section-title">Lampiran</div>
-            <table class="table-container">
-                <tr>
-                    <th>Proposal Teknis</th>
-                    <td>{{ $persetujuan->proposal_teknis }}</td>
-                </tr>
-                <tr>
-                    <th>Alur Proses Bisnis yang Existing</th>
-                    <td>{{ $persetujuan->alur_proses_existing }}</td>
-                </tr>
-                <tr>
-                    <th>Alur Proses Bisnis untuk Perbaikan atau Otomasi</th>
-                    <td>{{ $persetujuan->alur_proses_perbaikan }}</td>
-                </tr>
-                <tr>
-                    <th>Prosedur & Instruksi Kerja</th>
-                    <td>{{ $persetujuan->prosedur_instruksi }}</td>
-                </tr>
-            </table>
+<h3 class="text-center bold">INFO KEBUTUHAN SISTEM INFORMASI</h3>
+<table>
+    <tr>
+        <td>Latar Belakang</td>
+        <td>{{ $persetujuan->latar_belakang }}</td>
+    </tr>
+    <tr>
+        <td>Tujuan</td>
+        <td>{{ $persetujuan->tujuan }}</td>
+    </tr>
+    <tr>
+        <td>Target Implementasi Sistem</td>
+        <td>{{ $persetujuan->target_implementasi }}</td>
+    </tr>
+    <tr>
+        <td>Fungsi-fungsi Sistem Informasi</td>
+        <td>{{ $persetujuan->fungsi_sistem }}</td>
+    </tr>
+    <tr>
+        <td>Jenis Aplikasi</td>
+        <td>{{ $persetujuan->jenis_aplikasi }}</td>
+    </tr>
+    <tr>
+        <td>Pengguna</td>
+        <td>{{ $persetujuan->pengguna }}</td>
+    </tr>
+    <tr>
+        <td>Uraian Permintaan Tambahan/Khusus</td>
+        <td>{{ $persetujuan->uraian_tambahan }}</td>
+    </tr>
+    <tr>
+        <td>Lampiran</td>
+        <td>{{ $persetujuan->lampiran}}</td>
+    </tr>
+</table>
 
-            <div class="section-title">Status Persetujuan</div>
-            <table class="table-container">
-                <tr>
-                    <th>Alasan</th>
-                    <td>{{ $persetujuan->alasan_persetujuan }}</td>
-                </tr>
-                <tr>
-                    <th>Status</th>
-                    <td>{{ $persetujuan->status_persetujuan }}</td>
-                </tr>
-            </table>
+<h3 class="text-center bold">PERSETUJUAN KEBUTUHAN SISTEM INFORMASI</h3>
+<table>
+    <tr>
+        <td>Status Persetujuan</td>
+        <td>{{ $persetujuan->status_persetujuan}}</td>
+    </tr>
+    <tr>
+        <td>Alasan</td>
+        <td>{{ $persetujuan->alasan_persetujuan}}</td>
+    </tr>
+</table>
+<table class="table">
+    <tr>
+        <th class="text-center" colspan="2">Disiapkan oleh</th>
+        <th class="text-center" colspan="2">Disetujui oleh</th>
+    </tr>
+    <tr>
+        <td colspan="2" style="height: 100px;"></td>
+        <td colspan="2" style="height: 100px;"></td>
+    </tr>
+    <tr>
+        <td class="text-center" colspan="2">{{ $persetujuan->namapemohon }}<br>{{$persetujuan->jabatanpeninjau}}</td>
+        <td class="text-center" colspan="2">{{ $persetujuan->namapeninjau }}<br>{{$persetujuan->namapenyetuju}}</td>
+    </tr>
+    <tr>
+        <td class="text-center" colspan="2">Tanggal: {{ \Carbon\Carbon::parse($persetujuan->tanggal_disiapkan)->format('d-m-Y') }}</td>
+        <td class="text-center" colspan="2">Tanggal: {{ \Carbon\Carbon::parse($persetujuan->tanggal_disetujui)->format('d-m-Y') }}</td>
+    </tr>
+</table>
 
-            <div class="section-title">Persetujuan oleh</div>
-            <table class="table-container">
-                <tr>
-                    <th>Nama Pemohon</th>
-                    <td>{{ $persetujuan->nama_pemohon }}</td>
-                </tr>
-                <tr>
-                    <th>Nama Peninjau</th>
-                    <td>{{ $persetujuan->nama_peninjau }}</td>
-                </tr>
-                <tr>
-                    <th>Jabatan Peninjau</th>
-                    <td>{{ $persetujuan->jabatan_peninjau }}</td>
-                </tr>
-                <tr>
-                    <th>Nama Penyetuju</th>
-                    <td>{{ $persetujuan->nama_penyetuju }}</td>
-                </tr>
-            </table>
-            <div class="text-right"><strong>Tanggal:</strong> {{ \Carbon\Carbon::parse($persetujuan->created_at)->format('d M, Y') }}</div>
-        </div>
+@if (!$loop->last)
+<div class="page-break"></div>
+@endif
 
-        @if ($loop->iteration % 3 == 0)
-            <div class="page-break"></div>
-        @endif
-    @endforeach
+@endforeach
+
 </body>
 </html>
